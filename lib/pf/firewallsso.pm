57ec49f5fe7229eafeec27130290be8089c706e6
Durand Fabrice <fdurand@inverse.ca>
2014-03-26 11:37:20 -0400

    Added object oriented module to deal with Fortigate and PaloAlto firewall

---
+++ lib/pf/firewallsso.pm
@@ -0,0 +1,103 @@
+package pf::firewallsso;
+
+=head1 NAME
+
+pf::web::firewallsso
+
+=cut
+
+=head1 DESCRIPTION
+
+pf::web::firewall. 
+
+=cut
+
+use strict;
+use warnings;
+
+use Log::Log4perl;
+use pf::config;
+use pf::util;
+
+=head1 SUBROUTINES
+
+=over
+
+=item new
+
+=cut
+
+sub new {
+   my $logger = Log::Log4perl::get_logger("pf::firewallsso");
+   $logger->debug("instantiating new pf::firewallsso");
+   my ( $class, %argv ) = @_;
+   my $self = bless {}, $class;
+   return $self;
+}
+
+=item do_sso
+
+Send the sso stuff to the firewall
+
+=cut
+
+sub do_sso {
+    my ($self, $method, $mac, $ip) = @_;
+    my $logger = Log::Log4perl::get_logger( ref($self) );
+
+    foreach my $firewall_conf ( sort keys %ConfigFirewallSSO ) {
+        $logger->warn($ConfigFirewallSSO{$firewall_conf}->{'type'});
+        my $module_name = 'pf::firewallsso::'.$ConfigFirewallSSO{$firewall_conf}->{'type'};
+        $module_name = untaint_chain($module_name);
+        # load the module to instantiate
+        if ( !(eval "$module_name->require()" ) ) {
+            $logger->error("Can not load perl module: $@");
+            return 0;
+        }
+        my $firewall = $module_name->new();
+        my $return = $firewall->action($firewall_conf,$method,$mac,$ip);
+    }
+}
+
+
+=back
+
+=head1 AUTHOR
+
+Inverse inc. <info@inverse.ca>
+
+Minor parts of this file may have been contributed. See CREDITS.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2005-2014 Inverse inc.
+
+Copyright (C) 2005 Kevin Amorin
+
+Copyright (C) 2005 David LaPorte
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+USA.
+
+=cut
+
+1;
+
+# vim: set shiftwidth=4:
+# vim: set expandtab:
+# vim: set backspace=indent,eol,start:
+
